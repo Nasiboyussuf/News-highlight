12028d11a7ea9deb90802c56f07dce9409667845
Nasibo Yussuf <yussufnasibo@gmail.com>
2019-06-24 13:59:27 +0300

    created virtual environment

---
+++ manage.py
@@ -0,0 +1,21 @@
+from app import create_app
+from flask_script import Manager, Server
+from flask import Flask
+
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+@manager.command
+def test():
+    '''
+    Run unit tests(this feature is the coolest bana)
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+# manager.add_command('server',Server)
+if __name__ == '__main__':
+    manager.run()
---
+++ start.sh
@@ -0,0 +1,3 @@
+export NEWS_API_KEY='9c51a715540a49349de5a8a5a70440e9'
+
+python3.6 manage.py server 
